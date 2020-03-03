6ea412d468cf06ba51c807819f2d83fcc499948f
Marcos Vinicius <marcosvinicius.aux@gmail.com>
2020-03-03 17:43:04 -0300

    tentativa de uso setState

--- wimape/src/SamplePath.js
+++ wimape/src/SamplePath.js
@@ -1,19 +1,28 @@
 import React from "react"
 import api from "./services/api"
 
-function SamplePath() {    
-        async function dbtest() { //comunicação funcionando
-        return await api.get(`/search`,{params:{num_serie_teste:"1"}})//.then(resp=>{return resp.data})
-        } 
+class SamplePath extends React.Component {   
+constructor(props) {
+    super(props)
+    this.state = {
+        teste: "teste"
+    }
+    }
 
+async dbtest() { //comunicação funcionando
+    await api.get(`/search`,
+    {params:{num_serie_teste:"1"}})
+    .then(resp=>{ this.setState( {teste: "deu"}) })
+    .catch(err => { this.setState( {teste: err}) })
+    } 
+handleChange = (prop) => {
+        this.setState({teste: prop})
+};
 
     //const responseData = dbtest().then( response => {return response.data}).catch(response => {return console.log(response)})
-    const cod = dbtest()
-    console.log(cod)
-    return (        
-        <div>
-            <p></p>
-        </div>
+render() { 
+    return ( <h1 onClick={this.handleChange("deu")}>{this.state.teste}</h1>
     )
+   }     
 }
 export default SamplePath
