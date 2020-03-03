1e0c49885718d9f947aa85f2aca2d082bedb19de
Marcos Vinicius <marcosvinicius.aux@gmail.com>
2020-03-03 16:33:22 -0300

    Comitando pequenas mudanças

--- wimape/src/SamplePath.js
+++ wimape/src/SamplePath.js
@@ -1,11 +1,18 @@
 import React from "react"
+import api from "./services/api"
 
-function SamplePath() {
-    return (
+function SamplePath() {    
+        async function dbtest() { //comunicação funcionando
+        return await api.get(`/search`,{params:{num_serie_teste:"1"}})//.then(resp=>{return resp.data})
+        } 
+
+
+    //const responseData = dbtest().then( response => {return response.data}).catch(response => {return console.log(response)})
+    const cod = dbtest()
+    console.log(cod)
+    return (        
         <div>
-            <div>
-                
-            </div>
+            <p></p>
         </div>
     )
 }
